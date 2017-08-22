c3a9505ab5d5525cbcb39733134f643470662a46
liyong <liyong757575@163.com>
2017-08-22 23:16:59 +0800

    fix

--- src/vuex/modules/home.js
+++ src/vuex/modules/home.js
@@ -1,8 +1,3 @@
-/**
- * author: Yank
- * date: 2017-03-06
- * usage: 购物车所需用到ACtions ,数据的计算在 view层完成
- */
 
 import * as types from './../mutation-types.js'
 
--- src/vuex/mutation-types.js
+++
@@ -1,6 +0,0 @@
-/**
- * author: Yank
- * date: 2017-03-06
- * usage: 购物车所需用到ACtions ,数据的计算在 view层完成
- */
-
